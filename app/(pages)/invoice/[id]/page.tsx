89393e808483e503e23da8af05dcbe52ec2be543
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-06-23 13:06:42 +0400

    add detailed page

--- app/(pages)/invoice/[id]/page.tsx
+++ app/(pages)/invoice/[id]/page.tsx
@@ -1,4 +1,5 @@
 import data from '../../../../data.json'
+import Image from 'next/image';
 
 type Props = {
     params: {
@@ -13,7 +14,23 @@ const invoice=data.find((inv)=>inv.id===id);
  
   return (
     <>
-      <div>{invoice?.id}blabla</div>
+    <main className='flex flex-col items-center'>
+      <button className='flex gap-3 justify-center items-center'>
+          <Image src='../../../../assets/icon-arrow-left.svg' alt='arrowleft' width={7} height={7}/>
+          <p>Go back</p>
+      </button>
+      <section className='w-10/12 flex items-center justify-between bg-[var(--color-white)] rounded-lg p-6' >
+         <div className='flex gap-2 justify-center items-center'>
+             <p className='text-[var(--color-dark-gray)] text-xs'>Status</p>
+             <div className={`status-${invoice?.status.toLowerCase()} text-md`}>{invoice?.status}</div>
+         </div>
+         <div className='flex justify-center items-center gap-3'>
+             <button className='bg-gray-100 rounded-3xl px-5 py-3'>Edit</button>
+             <button className='bg-[var(--color-dark-red)] rounded-3xl px-5 py-3'>Delete</button>
+             <button className='bg-[var(--color-dark-purple)] rounded-3xl px-5 py-3'>Mark as Paid</button>
+         </div>
+      </section>
+    </main>
     </>
   )
 }
